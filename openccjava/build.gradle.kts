01b2d688d763ce0ad45225aa0c012e90fdff1a1f
laisuk <laisuk@yahoo.com>
2025-07-30 15:26:22 +0800

    Release v1.0.0

--- openccjava/build.gradle.kts
+++ openccjava/build.gradle.kts
@@ -16,6 +16,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
 }
