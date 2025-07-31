79d7b2fc537e578be91b9c7e309f92680dc50355
laisuk <laisuk@yahoo.com>
2025-07-31 09:56:04 +0800

    Release v1.0.0

--- openccjavacli.bat
+++ openccjavacli.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set JRE=C:\Java\zulu11.82.19-ca-jdk11.0.28-win_x64\bin\java.exe
+set JRE=java
 set JAR=openccjavacli\build\libs\openccjavacli-1.0.0.jar
 set LIB=lib\*
 rem java -cp "%JAR%;%LIB%" openccjavacli.Main %*
