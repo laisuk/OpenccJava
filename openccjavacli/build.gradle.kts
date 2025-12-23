0b3bcfea8667b040339ade5cc31bd4bf923427ee
laisuk <laisuk@yahoo.com>
2025-12-23 17:03:39 +0800

    build Windows native

--- openccjavacli/build.gradle.kts
+++ openccjavacli/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
     // --- GraalVM Native Build Tools (AOT compile to a single exe) ---
     // Must use Oracle GraalVM JDK to build
-//    id("org.graalvm.buildtools.native") version "0.10.2"
+    id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "io.github.laisuk"
