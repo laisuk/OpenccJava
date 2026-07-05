1549a64191c6acc736c2b8151dd9023fdde3e6fe
laisuk <laisuk@yahoo.com>
2026-07-06 06:41:21 +0800

    Handling standalone dialog closer in line

--- openccjavacli/src/main/java/pdfboxhelper/PdfReflowHelper.java
+++ openccjavacli/src/main/java/pdfboxhelper/PdfReflowHelper.java
@@ -411,8 +411,9 @@ public final class PdfReflowHelper {
             //   missing opening quotes or cross-page broken quoted text
             if (strippedEndsWithDialogCloser) {
                 boolean punctBeforeCloserIsStrong =
-                        PunctSets.tryGetPrevNonWhitespace(stripped, lastIdxRef.index, prevRef) &&
-                                PunctSets.isClauseOrEndPunct(prevRef.value);
+                        (PunctSets.tryGetPrevNonWhitespace(stripped, lastIdxRef.index, prevRef)
+                                || PunctSets.tryGetLastNonWhitespace(bufferText, prevRef))
+                                && PunctSets.isClauseOrEndPunct(prevRef.value);
 
                 buffer.append(stripped);
                 dialogState.update(stripped);
