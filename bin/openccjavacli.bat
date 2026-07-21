c202e2649ca21f6eeb2dff3c8b1e5017b2cf296c
laisuk <laisuk@yahoo.com>
2026-07-22 02:29:09 +0800

    Optimized batch scripts

--- bin/openccjavacli.bat
+++ bin/openccjavacli.bat
@@ -1,18 +1,28 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 
-set "BASE=%~dp0.."
+rem Use UTF-8 for Unicode command-line arguments and console output.
+chcp 65001 >nul
+
 set "JRE=java"
-set "LIB=%BASE%\lib\*"
+set "LIB=lib\*"
 
-rem Find the newest JAR inside openccjavacli\build\libs
-for /f "delims=" %%F in ('dir /b /o-d "%BASE%\openccjavacli\build\libs\openccjavacli-*.jar" 2^>nul') do (
-    set "JAR=%BASE%\openccjavacli\build\libs\%%F"
+rem Find the newest CLI JAR by modification time.
+for /f "delims=" %%F in ('dir /b /a-d /o-d "openccjavacli\build\libs\openccjavacli-*.jar" 2^>nul') do (
+    set "JAR=openccjavacli\build\libs\%%F"
     goto :found
 )
 
-echo Error: No JAR found in %BASE%\openccjavacli\build\libs
+echo Error: No JAR found in openccjni-cli\build\libs
+endlocal
 exit /b 1
 
 :found
-"%JRE%" -cp "%JAR%;%LIB%" openccjavacli.Main %*
+"%JRE%" ^
+  -Dfile.encoding=UTF-8 ^
+  -Dsun.jnu.encoding=UTF-8 ^
+  -cp "%JAR%;%LIB%" ^
+  openccjavacli.Main %*
+
+set "EXIT_CODE=%ERRORLEVEL%"
+endlocal & exit /b %EXIT_CODE%
