f6670e82a1dbeeb67dbf6e92e8da1134552942c5
laisuk <laisuk@yahoo.com>
2026-08-13 03:26:51 +0800

    Update GraaVM Meta JSON generation YML

--- openccjavacli/build.gradle.kts
+++ openccjavacli/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     application
     // --- GraalVM Native Build Tools (AOT compile to a single exe) ---
     // Must use Oracle GraalVM JDK to build
-    id("org.graalvm.buildtools.native") version "1.1.8"
+//    id("org.graalvm.buildtools.native") version "0.11.5"
 }
 
 group = "io.github.laisuk"
@@ -161,6 +161,7 @@ tasks.register<Exec>("verifyFatJarSig") {
 
 // --- Uncomment these if using GraalVM native image ---
 // Must use Oracle GraalVM JDK to build
+/*
 
 val osKey = org.gradle.nativeplatform.platform.internal.DefaultNativePlatform.getCurrentOperatingSystem().run {
     when {
@@ -313,3 +314,4 @@ val generateNativeImageJson by tasks.registering(JavaExec::class) {
         "-r"
     )
 }
+*/
